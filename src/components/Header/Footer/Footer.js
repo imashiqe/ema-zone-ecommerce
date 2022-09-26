d39f89b7abf87388c028e823fc008ec08538baec
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-27 02:01:31 +0600

    Footer added

---
+++ src/components/Header/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <p>CC 2022  Developed By imashiqe</p>
+        </div>
+    );
+};
+
+export default Footer;
