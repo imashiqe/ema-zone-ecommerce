551ffc8c43d33fcdfce6e085cff1ab9c1c3f8576
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-27 01:43:46 +0600

    shopping cart calculation

--- src/components/Header/Cart/Cart.js
+++ src/components/Header/Cart/Cart.js
@@ -4,17 +4,21 @@ const Cart = (props) => {
      const {cart} = props;
 
      let total = 0;
+     let shipping = 0;
+
      for(const product of cart){
         total = total + product.price;
+        shipping = shipping + product.shipping;
      }
- 
+    
+     const tax = (total * 10/100).toFixed(2);
     return (
         <div className='cart'>
             <h3>Order Summery</h3>
             <p>Selected Item {props.cart.length} </p> 
-            <p>Total Price : {total}</p>
-            <p>Shipping : </p>
-            <p>Tax :</p>
+            <p>Total Price : ${total} </p>
+            <p>Shipping : ${shipping} </p>
+            <p>Tax : ${tax}</p>
             <h3>Grand Total : </h3>
         </div>
     );
