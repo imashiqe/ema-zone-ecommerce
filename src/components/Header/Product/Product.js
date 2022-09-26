fcbae432189367c8818c55cb2e104d51339995df
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-26 18:19:41 +0600

    button added

--- src/components/Header/Product/Product.js
+++ src/components/Header/Product/Product.js
@@ -7,7 +7,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt=""></img>
+            <div className='product-info'>
             <p className='product-name'>{name}</p>
+            <p>Price:{price} $ </p>
+            <p><small>Seller: {seller}</small></p>
+            <p>Ratings: {ratings} stars</p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
