34779d3cdc5bb24285b1cbbe8738e118fc62b75f
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-27 01:34:58 +0600

    total  product price added

--- src/components/Header/Cart/Cart.js
+++ src/components/Header/Cart/Cart.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = (props) => {
+     const {cart} = props;
+
+     let total = 0;
+     for(const product of cart){
+        total = total + product.price;
+     }
+ 
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summery</h3>
             <p>Selected Item {props.cart.length} </p> 
+            <p>Total Price : {total}</p>
+            <p>Shipping : </p>
+            <p>Tax :</p>
+            <h3>Grand Total : </h3>
         </div>
     );
 };
