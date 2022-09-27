2a51cd35ef393c822d4cc7d25f419bfb19fd7362
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-27 19:42:03 +0600

    data added localstorage fakedb

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -8,16 +9,40 @@ const Shop = () => {
 
     const [cart,setCart] = useState([]);
     useEffect( () => {
+        console.log('Products load before fetch')
         fetch('products.json')
         .then(res=> res.json())
-        .then(data => setProducts(data))
+        .then(data => {
+            setProducts(data);
+            console.log('product loaded')
+        })
     },[]);
+   
+    useEffect(()=>{
+        console.log('local storage first line')
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in  storedCart){
+
+            const addedProduct = products.find(product=> product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+
+        }
+    setCart(savedCart);
+
+    },[products])
+
 
     const handleAddToCart = (product) => {
         console.log(product);
         // cart.push(product);
         const newCart =  [...cart,product];
         setCart(newCart);
+        addToDb(product.id)
      }
 
 
